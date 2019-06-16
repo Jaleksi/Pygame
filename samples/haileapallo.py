ece97af91792d6231ca6bd865e93cb11b01d6abf
Aleksi <aleksi.jok@gmail.com>
2019-06-16 17:55:12 +0300

    haileapallo.py

---
+++ samples/haileapallo.py
@@ -0,0 +1,80 @@
+import pygame
+
+pygame.init()
+leveys = 400
+korkeus = 300
+
+display = pygame.display.set_mode((leveys, korkeus))
+pygame.display.set_caption("WoWzA")
+clock = pygame.time.Clock()
+
+
+class Pallo:
+    def __init__(self, x, y, vari):
+        self.x = x
+        self.y = y
+        self.vari = vari
+        self.vauhti = 3
+        self.xsuunta = 1
+        self.ysuunta = 0
+        self.loc = []
+
+    def piirto(self):
+        for sij in self.loc:
+            pygame.draw.circle(display, sij[2], [sij[0], sij[1]], 10, 1)
+            sij[2][1] += 10
+            sij[2][2] += 10
+
+    def liiku(self):
+        self.loc.append([self.x, self.y, [250, self.vari, self.vari]])
+
+        if(len(self.loc) > 25):
+            self.loc.pop(0)
+
+        if(self.xsuunta == 0):
+            self.x += self.vauhti
+        else:
+            self.x -= self.vauhti
+        if(self.ysuunta == 0):
+            self.y += self.vauhti
+        else:
+            self.y -= self.vauhti
+
+        if(self.x >= 390 or self.x <= 10):
+            self.xsuunta = vaihto(self.xsuunta)
+
+        if(self.y >= 290 or self.y <= 10):
+            self.ysuunta = vaihto(self.ysuunta)
+
+
+def vaihto(x):
+    if(x == 0):
+        return 1
+    else:
+        return 0
+
+
+def inputt():
+    for event in pygame.event.get():
+        if(event.type == pygame.QUIT):
+            pygame.quit()
+            exit()
+
+
+def logic(entity):
+    entity.liiku()
+    entity.piirto()
+
+
+def main():
+    pallo = Pallo(177, 140, 0)
+    while(True):
+        inputt()
+        display.fill(pygame.Color("white"))
+        logic(pallo)
+        pygame.display.update()
+        clock.tick(30)
+
+
+if(__name__ == "__main__"):
+    main()
